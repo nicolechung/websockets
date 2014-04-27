dcf1b6c8fe30510f230073c3f019b31fa5315d20
Nicole Chung <nchung@nascentdigital.com>
2014-04-27 17:31:40 -0400

    moved socket stuff over to script.js, added Angular controller

--- site/js/script.js
+++ site/js/script.js
@@ -1,1 +1,13 @@
 /* Angular stuff goes here */
+var socket = io.connect('http://localhost');
+
+socket.on('news', function (data) {
+  console.log(data);
+  socket.emit('my other event', {
+    my: 'data'
+  });
+});
+
+function BeforeIDoCtrl($scope) {
+
+}
